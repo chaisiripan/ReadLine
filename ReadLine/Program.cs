9d77fcec0193c050ab5afdc50601fbe0b486165d
Pongpat Chaisiripan <pongpat.cha@siam.edu>
2024-02-08 11:00:30 +0700

    Add project files.

---
+++ ReadLine/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter you name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter you age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine("You name is {0} and you age is {1} year olds", name, age);
+        }
+    }
+}
